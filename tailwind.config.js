6a690b28b37c213107995435f4168ec8f7b3a7df
Akshay Kamble <akshay.kamble@copods.co>
2023-03-01 20:34:37 +0530

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'geekblue':{
+          100:'#f0f5ff',
+          200:'#d6e4ff',
+          300:'#adc6ff',
+          400:'#85a5ff',
+          500:'#597ef7',
+          600:'#2f54eb',
+          700:'#1d39c4',
+          800:'#10239e',
+          900:'#061178',
+          1000:'#061178'
+        },
+        'sunsetOrange':{
+          200:'#ffe7ba'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
