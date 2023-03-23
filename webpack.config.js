620935148cb785aea4a84f83eedeffc32c08d751
Akshay Kamble <akshay.kamble@copods.co>
2023-03-23 15:53:33 +0530

    initial-commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: "http://localhost:3105/",
   },
 
   resolve: {
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 3000,
+    port: 3105,
     historyApiFallback: true,
   },
 
@@ -41,7 +41,7 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "template_mfe",
+      name: "workflow_mfe",
       filename: "remoteEntry.js",
       remotes: {},
       exposes: {},
