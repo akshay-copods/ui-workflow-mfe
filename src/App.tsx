149deff500e6e50ed03a4a54d65887274108ccae
Akshay Kamble <akshay.kamble@copods.co>
2023-03-28 16:47:43 +0530

    add: panel with state

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
-import { Button, Drawer, Input } from "antd";
+import { Drawer, Input } from "antd";
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { ReactFlow } from "reactflow";
 
-import ReactFlow from "reactflow";
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
-import "reactflow/dist/style.css";
-
+const httpData = {
+  defaultDomain: "https://sandboxurl.com/dev7-zsddls.com/signup",
+  isCustomDomain: true,
+  triggerType: [
+    "HTTP based trigger",
+    "SMTP based trigger",
+    "FTP based trigger",
+  ],
+  customDomain: {
+    appEnv: ["Dev", "Staging", "Prod"],
+    url: "https://test.dev.com",
+    urlPath: "",
+    params: [
+      { key: "plan", value: "standerd" },
+      { key: "search", value: "true" },
+    ],
+  },
+};
 const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
   {
     id: "1",
     position: { x: 0, y: 0 },
-    data: { label: "1" },
-    children: [
-      {
-        id: "1-1",
-        position: { x: 50, y: 0 },
-      },
-    ],
+    data: { label: "HTTP TRIGGER", type: "HTTP", data: httpData },
   },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    data: { label: "AUTHENTICATE", type: "AUTH" },
+  },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 const App = () => {
   const [open, setOpen] = useState(false);
+  const [panel, setPanel] = useState("");
 
   const showDrawer = () => {
     setOpen(true);
@@ -32,25 +49,29 @@ const App = () => {
   const onClose = () => {
     setOpen(false);
   };
+
+  const onNodeClick = (event, node) => {
+    setPanel(node.data.type);
+    console.log("click node", node);
+  };
+
   return (
     <div className='mt-10 text-3xl w-full h-screen'>
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <Button
-          type='primary'
-          onClick={showDrawer}>
-          Open
-        </Button>
+      {
         <Drawer
           title='Config'
           placement='right'
           onClose={onClose}
-          open={open}>
+          open={panel === "HTTP"}>
           <label htmlFor='Domain'>Domain</label>
           <Input />
         </Drawer>
+      }
+      <div style={{ width: "100vw", height: "100vh" }}>
         <ReactFlow
           nodes={initialNodes}
           edges={initialEdges}
+          onNodeClick={onNodeClick}
         />
       </div>
     </div>
