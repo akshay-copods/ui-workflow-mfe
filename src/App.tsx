4b85bb2f4551d59f36fa1455e71114c0202cc509
Akshay Kamble <akshay.kamble@copods.co>
2023-03-28 16:37:19 +0530

    add: update panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,60 @@
-import React from "react";
+import { Button, Drawer, Input } from "antd";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import ReactFlow from "reactflow";
-import "./index.scss";
 
 import "reactflow/dist/style.css";
 
-const onNodeClick = (event, node) => console.log('click node', node);
-
-const httpData = {
-  defaultDomain: "https://sandboxurl.com/dev7-zsddls.com/signup",
-  isCustomDomain: true,
-  triggerType: ["HTTP based trigger", "SMTP based trigger", "FTP based trigger"],
-  customDomain : {
-    appEnv: ["Dev", "Staging", "Prod"],
-    url: "https://test.dev.com",
-    urlPath: "",
-    params : [
-      {key: "plan", value: "standerd"},
-      {key: "search", value: "true"}
-    ]
-  }
-}
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "Label 1", type: "HTTP", data: httpData}},
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "Label 2", type: "SMTP"}},
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "1" },
+    children: [
+      {
+        id: "1-1",
+        position: { x: 50, y: 0 },
+      },
+    ],
+  },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-const App = () => (
-  <div className='mt-10 text-3xl mx-auto max-w-6xl'>
-    <div className='h-96 w-96 border-8'>
-      <ReactFlow
-        nodes={initialNodes}
-        edges={initialEdges}
-        onNodeClick={onNodeClick}
-      />
+
+const App = () => {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className='mt-10 text-3xl w-full h-screen'>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Button
+          type='primary'
+          onClick={showDrawer}>
+          Open
+        </Button>
+        <Drawer
+          title='Config'
+          placement='right'
+          onClose={onClose}
+          open={open}>
+          <label htmlFor='Domain'>Domain</label>
+          <Input />
+        </Drawer>
+        <ReactFlow
+          nodes={initialNodes}
+          edges={initialEdges}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
