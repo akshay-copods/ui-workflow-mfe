a3937fa738059f6e3e681806d5cde400d2f511af
shripaprikar <shrikant@copods.co>
2023-03-28 11:22:52 +0530

    Nodes
    Workflow Nodes

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,25 @@ import "./index.scss";
 
 import "reactflow/dist/style.css";
 
+const onNodeClick = (event, node) => console.log('click node', node);
+
+const httpData = {
+  defaultDomain: "https://sandboxurl.com/dev7-zsddls.com/signup",
+  isCustomDomain: true,
+  triggerType: ["HTTP based trigger", "SMTP based trigger", "FTP based trigger"],
+  customDomain : {
+    appEnv: ["Dev", "Staging", "Prod"],
+    url: "https://test.dev.com",
+    urlPath: "",
+    params : [
+      {key: "plan", value: "standerd"},
+      {key: "search", value: "true"}
+    ]
+  }
+}
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "Label 1", type: "HTTP", data: httpData}},
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "Label 2", type: "SMTP"}},
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 const App = () => (
@@ -17,8 +33,10 @@ const App = () => (
       <ReactFlow
         nodes={initialNodes}
         edges={initialEdges}
+        onNodeClick={onNodeClick}
       />
     </div>
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
